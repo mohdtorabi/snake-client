3882736418f2581c958af5f5d627bb04d5d1b1e9
mohdtorabi <mitq8.mt@gmail.com>
2020-11-04 22:52:49 +0000

    fixed and ready to move on

--- play.js
+++ play.js
@@ -4,29 +4,12 @@
  * Establishes connection with the game server
  */
 
-
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
 console.log('Connecting ...');
 
 connect();
-
-const handleUserInpu = function(data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-};
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", (data) => {
-
-    handleUserInpu(data);
-  });
-  return stdin;
-};
 setupInput();
  
 
